f661ff0da2b48bd4eaedcc2c3e15b922ec4f400a
anishdass <anishdassatoffice@gmail.com>
2025-04-12 16:21:10 +0100

    Hero section partially done without the 3d image

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from "react";
+import Hero from "./sections/Hero";
+
 
 const App = () => {
-  return (
-    <main>
-      <div>
-        <h1>Welcome to my 3D Portfolio!</h1>
-      </div>
-    </main>
-  );
+  return <Hero />;
 };
 
 export default App;
