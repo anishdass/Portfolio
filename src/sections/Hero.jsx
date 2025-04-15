3ab510e9e9c98978e29b0a42d7d9c27d64913739
anishdass <anishdassatoffice@gmail.com>
2025-04-15 14:22:40 +0100

    discontinuing the project because of system limitations

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,8 +3,17 @@ import { words } from "../constants";
 import Button from "../components/Button";
 import HeroExperience from "../components/HeroModels/HeroExperience";
 import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import AnimatedCounter from "../components/AnimatedCounter";
 
 const Hero = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".hero-text h1",
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, stagger: 0.2, duration: 5, ease: "power2.InOut" }
+    );
+  });
   return (
     <section id='hero' className='relative overflow-hidden'>
       <div className='absolute to-0 left-0 z-10'>
@@ -53,6 +62,7 @@ const Hero = () => {
           </div>
         </figure>
       </div>
+      <AnimatedCounter />
     </section>
   );
 };
